d4b8d7bf81270ffb77d69608b7f138b85a9df0ca
Doggenn <19arg09@gmail.com>
2024-04-12 17:36:54 +0200

    Añadidas nuevas funciones

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -2,51 +2,59 @@
 
 namespace App\Controller;
 
+use App\Entity\Pokemons;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PokemonController extends AbstractController
 {
 
-    #[Route('/pokemon')]
-    public function showPokemon()
+    #[Route('/pokemon/{id}', name: "showPokemon")]
+    public function showPokemon(EntityManagerInterface $doctrine, $id)
     {
 
-        $pokemon = [
-            'nombre' => 'pikachu',
-            'descripcion' => 'electrico',
-            'imagen' => 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/7/77/latest/20150621181250/Pikachu.png/800px-Pikachu.png',
-            'codigo' => '23456'
-        ];
+        $repositorio = $doctrine->getRepository(Pokemons::class);
+
+        $pokemon = $repositorio->find($id);
 
         return $this->render('pokemon/showPokemon.html.twig', ['pokemon' => $pokemon]);
     }
 
-    #[Route('/pokemons')]
-    public function listPokemon()
+    #[Route('/pokemons', name: "listPokemon")]
+    public function listPokemon(EntityManagerInterface $doctrine)
     {
 
-        $pokemons = [
-            [
-                'nombre' => 'pikachu',
-                'descripcion' => 'electrico',
-                'imagen' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
-                'codigo' => '23456'
-            ],
-            [
-                'nombre' => 'toggepi',
-                'descripcion' => 'psíquico',
-                'imagen' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/028.png',
-                'codigo' => '23457'
-            ],
-            [
-                'nombre' => 'squirtle',
-                'descripcion' => 'agua',
-                'imagen' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/021.png',
-                'codigo' => '23458'
-            ]
-        ];
+        $repositorio = $doctrine->getRepository(Pokemons::class);
+
+        $pokemons = $repositorio->findAll();
 
         return $this->render('pokemon/listPokemon.html.twig', ['pokemons' => $pokemons]);
+    }
+    #[Route('/insert/pokemon')]
+    public function insertPokemon(EntityManagerInterface $doctrine)
+    {
+        $pokemon = new Pokemons();
+        $pokemon->setNombre('pikachu');
+        $pokemon->setDescripcion('electrico');
+        $pokemon->setImagen('https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png');
+        $pokemon->setCodigo(23456);
+        $pokemon2 = new Pokemons();
+        $pokemon2->setNombre('toggepi');
+        $pokemon2->setDescripcion('psíquico');
+        $pokemon2->setImagen('https://assets.pokemon.com/assets/cms2/img/pokedex/full/028.png');
+        $pokemon2->setCodigo(23457);
+        $pokemon3 = new Pokemons();
+        $pokemon3->setNombre('squirtle');
+        $pokemon3->setDescripcion('agua');
+        $pokemon3->setImagen('https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png');
+        $pokemon3->setCodigo(23458);
+        $doctrine->persist($pokemon);
+        $doctrine->persist($pokemon2);
+        $doctrine->persist($pokemon3);
+        $doctrine->flush();
+        return new Response('¡Pokemon añadidos correctamente!');
     }
 }
