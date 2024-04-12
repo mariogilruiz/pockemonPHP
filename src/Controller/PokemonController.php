55823d9f88482c0a3f1eb0eae7dcd9ba2558c105
Andrea-GV <andrea.garcia.valverde@bootcamp-upgrade.com>
2024-04-12 15:16:37 +0200

    añadido listado de pokemon

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -21,4 +21,32 @@ class PokemonController extends AbstractController
 
         return $this->render('pokemon/showPokemon.html.twig', ['pokemon' => $pokemon]);
     }
+
+    #[Route('/pokemons')]
+    public function listPokemon()
+    {
+
+        $pokemons = [
+            [
+                'nombre' => 'pikachu',
+                'descripcion' => 'electrico',
+                'imagen' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+                'codigo' => '23456'
+            ],
+            [
+                'nombre' => 'toggepi',
+                'descripcion' => 'psíquico',
+                'imagen' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/028.png',
+                'codigo' => '23457'
+            ],
+            [
+                'nombre' => 'squirtle',
+                'descripcion' => 'agua',
+                'imagen' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/021.png',
+                'codigo' => '23458'
+            ]
+        ];
+
+        return $this->render('pokemon/listPokemon.html.twig', ['pokemons' => $pokemons]);
+    }
 }
